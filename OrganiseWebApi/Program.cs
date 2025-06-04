66f3a4085a12f5c894776b5bfa0504983d9c1abf
Marcos <marcoslaranz@gmail.com>
2025-06-05 10:43:58 +1200

    Update Program.cs
    Adding commentary

--- OrganiseWebApi/Program.cs
+++ OrganiseWebApi/Program.cs
@@ -1,20 +1,30 @@
+using OrganiseWebApi.Services;
 using OrganiseWebApi.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+//This allows the WeatherService to be injected
+//into any class/method that needs it.
+builder.Services.AddScoped<WeatherService>();
+// As Scoped this class will be instantiated 
+// for every request and will last 
+// till the processing the whole request
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); //This will allows us to see the OpenApi specification document
+	                  // using this link: http://localhost:5003/openapi/v1.json
 }
 
 app.UseHttpsRedirection();
+
+//This will run the Extension
+//class/method.
 app.MapWeatherEndPoints();
 
 app.Run();
